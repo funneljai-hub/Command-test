5721f090cde84154cebf56e9532b27e5ba562455
funneljai212 <93401587+funneljai212@users.noreply.github.com>
2024-02-23 13:27:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+const RANK_PREFIX = "rank:"
+const DEFAULT_RANK = "§bMember"
+export function getRanks(player) {
+    const ranks = player
+        .getTags()
+        .map((v) => {
+            if (!v.startsWith(RANK_PREFIX))
+                return null;
+            return v.substring(RANK_PREFIX.length);
+        })
+        .filter((x) => x);
+    return ranks.length == 0 ? [DEFAULT_RANK] : ranks;
+}
